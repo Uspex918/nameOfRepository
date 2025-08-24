59e66d4a15b1ae31f9d94394d895c1445dd0f6eb
Uspex918 <uspex964@gmail.com>
2025-08-24 17:01:16 +0400

    что бы не

--- js/script3.js
+++ js/script3.js
@@ -1,1 +1,27 @@
-let a = 6;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+}
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+
+
+
+
+
+
+console.log(personalMovieDB);
+console.log(typeof(numberOfFilms));
