841a477ce9e27a27fee8a4a61cd65cce46e00cc4
Uspex918 <uspex964@gmail.com>
2025-08-26 21:56:16 +0400

    today

---
+++ js/script4.js
@@ -0,0 +1,126 @@
+// if (5){
+//     console.log("Ok!");
+// } else {
+//     console.log("Not Ok!");
+// }
+
+// const num = 120;
+
+// if (num < 50) {
+//     console.log("<50");
+// } else if (num > 100){
+//     console.log(">100");
+// } else {
+//     console.log(">= 50 but <= 100");
+// }
+
+    
+// (num == 101)? console.log("yes = 101") : console.log("no");
+
+// switch (num) {
+//     case 100:
+//         console.log("100");
+//         break;
+//     case 120:
+//         console.log("120");
+//         break;
+//     case 101:
+//         console.log("101");
+//         break;
+//     default:
+//         console.log("Default");
+//         break;
+// }
+
+
+
+
+// console.log( NaN || 2 || undefined);
+
+
+// console.log( undefined && NaN );
+
+
+// console.log( 1 && 2 && 3 );
+
+
+// console.log( !1 && 2 || !3 );
+
+
+// console.log( 25 || null && !3 );
+
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// const hamburger = 0;
+// const fries = null;
+// const cola = 0;
+
+// console.log(hamburger || cola || fries);
+
+
+// if (hamburger === 3 && cola === 1 && fries){
+//     console.log("Все сыты!");
+// } else {
+//     console.log("Уходим!");
+// }
+// console.log(!" ");
+
+// for (let i = 0; i < 5; i++) {
+//   console.log('Счётчик равен: ' + i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     console.log('text')
+//     break
+//   }
+//   console.log(i)
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     console.log('continue')
+//     continue
+//   }
+//   console.log(i)
+// }
+
+const numbers = [1, 2, 3, 4, 5]
+// let i = 0
+// while (i < numbers.length) {
+//   const currentElement = numbers[i]
+//   console.log(currentElement + currentElement)
+//   i++
+// }
+// напечатает 1, 4, 9, 16, 25
+
+// let count = numbers.length
+// while (count) {
+//   // вариант с приведением типов в условии цикла
+//   console.log(count)
+//   count--
+// }
+// напечатает 5, 4, 3, 2, 1
+
+// do {
+//   console.log(count)
+//   count--
+// } while (count > 0);
+
---
+++ js/script5.js
@@ -0,0 +1,22 @@
+let num = 50;
+
+// while(num <= 55){
+//     console.log(num);
+//     num++;
+// }
+//     console.log(num);
+
+
+
+// let num2 = 50;
+// do {
+//     console.log(num2);
+//     num2++;
+// } while (num2 <= 55);
+
+//     console.log(num2);
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+}
